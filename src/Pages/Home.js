ad1a7726ae6ebe62ad957b1119a24908d5d2c5c5
richad.97 <rcastillo2022@gmail.com>
2021-11-13 13:33:53 -0800

    updated home order button

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,15 @@
 import { FaPizzaSlice } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <section id="home-section">
       <div id="banner-container"></div>
       <div id="order-container">
-        <div>
+        <Link to="/pizza">
           <FaPizzaSlice id="pizza-icon" />
           <p>Order Now</p>
-        </div>
+        </Link>
       </div>
     </section>
   );
